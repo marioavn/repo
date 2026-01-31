4de7b991bb5575ce43476e43c1fa601f590ddac6
Mario Andrés Vizcaino Navarro <mandresvizcaino@unibarranquilla.edu.co>
2026-01-31 16:47:59 -0500

    codigosenc.py
    algoritmo sencillo en python

---
+++ sencillo.py
@@ -0,0 +1,13 @@
+""" programa sencillo para calcular problema aritmetico con numeros enteros"""
+
+x = int(input("Ingrese un numero: "))
+y = int(input("Ingrese el segundo numero: "))
+
+suma= x + y
+resta= x - y
+multiplicacion= x * y
+
+
+print("La suma de los dos numeros es: ",suma)
+print("La resta de los dos numeros es: ",resta)
+print("La multiplicacion de los dos numeros es: ",multiplicacion)
